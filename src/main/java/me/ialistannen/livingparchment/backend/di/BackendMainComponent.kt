1b4e42d26f4c32004cccf4891c481e30b9e32f08
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-05-19 23:39:17 +0200

    Fix the fetching module not being depended on by the main component

--- src/main/java/me/ialistannen/livingparchment/backend/di/BackendMainComponent.kt
+++ src/main/java/me/ialistannen/livingparchment/backend/di/BackendMainComponent.kt
@@ -1,11 +1,7 @@
 package me.ialistannen.livingparchment.backend.di
 
 import dagger.Component
-import me.ialistannen.livingparchment.backend.storage.BookRepository
 
 @ApplicationScope
-@Component(modules = [BookRepositoryModule::class, JdbiModule::class])
-abstract class BackendMainComponent {
-
-    abstract fun getBookRepository(): BookRepository
-}
+@Component(modules = [BookRepositoryModule::class, JdbiModule::class, FetchingModule::class])
+abstract class BackendMainComponent
